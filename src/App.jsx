2780fd119f74c49b32319f5c751055cd23db37b2
Mayur Marwade <mayumarwade899@gmail.com>
2024-07-26 18:43:39 +0530

    gemini clone added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from "react"
+import Sidemenu from "./components/Sidemenu/Sidemenu"
+import Main from "./components/Main/Main"
+
+const App = () => {
+  return (
+    <>
+      <Sidemenu />
+      <Main />
+    </>
+  )
+}
+
+export default App
